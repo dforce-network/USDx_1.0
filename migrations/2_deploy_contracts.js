bbb40006b6065de6b270e2ab341843ed286143e9
snow <snow@dforce.network>
2019-05-27 12:46:34 +0800

    DF deployed already. just use it when one-key deploy in "Rinkeby". set rate for deposit/destroy. set medianizer and price.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,28 +8,28 @@ const Guard = artifacts.require('DSGuard.sol');
 const PriceFeed = artifacts.require('PriceFeed.sol');
 const Medianizer = artifacts.require('Medianizer.sol');
 const USDx = artifacts.require('USDXToken.sol');
-const DF = artifacts.require('DFToken.sol');
+// const DF = artifacts.require('DFToken.sol');
 
-const USDx_Addr = "0x17996ea27d03d68ddc618f9b8f0faf43838acaf6";
-const DFN_Addr = "0xfaacf3d2a2ce1102073038e035d24c1c78b6e9c4";
+// const USDx_Addr = "0x17996ea27d03d68ddc618f9b8f0faf43838acaf6";
+const DF_Addr = "0x4AF82b7C2F049574C9fc742A896DAbEA379b7d51";
 
 module.exports = async function (deployer, network, accounts) {
     await deployer.deploy(USDx, "0x6b6b00000000000000000000000000");
-    await deployer.deploy(DF, "0x6b6600000000000000000000000000");
+    // await deployer.deploy(DF, "0x6b6600000000000000000000000000");
     await deployer.deploy(Guard);
     await deployer.deploy(Protocol);
     await deployer.deploy(Store,
         [
-            '0x235b370de0b0cd3fb9e987e4957a9db0f1c3dd5a',
-            '0x9aa0fa0a4e2634fbbf1b716fcabf8650a8fa330f',
-            '0xebb02dbf58104b93af2a89ae55ef2d7a7be36247',
-            '0x676ce98a3bc9c191903262f887bb312ce20f851f'
-        ], [100, 200, 300, 400]);
+            '0xf494e07dfdbce883bf699cedf818fde2fa432db4',
+            '0x561b11000e95ac053eccec5bcefdc37e16c2491b',
+            '0x25470030aa105bca679752e5c5e482c295de2b68',
+            '0xbc34e50f589e389c507e0213501114bd2e70b1d7'
+        ], [10, 20, 30, 40]);
     await deployer.deploy(Collateral);
-    await deployer.deploy(Funds, "0xfaacf3d2a2ce1102073038e035d24c1c78b6e9c4");
+    await deployer.deploy(Funds, DF_Addr);
     await deployer.deploy(Pool, Collateral.address);
     await deployer.deploy(Medianizer);
     await deployer.deploy(PriceFeed);
-    await deployer.deploy(Engine, "0x17996ea27d03d68ddc618f9b8f0faf43838acaf6", "0xfaacf3d2a2ce1102073038e035d24c1c78b6e9c4",
+    await deployer.deploy(Engine, USDx.address, DF_Addr,
         Store.address, Pool.address, Collateral.address, Funds.address, Medianizer.address);
 };
--- migrations/3_after_deploy.js
+++ migrations/3_after_deploy.js
@@ -10,9 +10,9 @@ const Guard = artifacts.require('DSGuard.sol');
 const PriceFeed = artifacts.require('PriceFeed.sol');
 const Medianizer = artifacts.require('Medianizer.sol');
 const USDx = artifacts.require('USDXToken.sol');
-const DF = artifacts.require('DFToken.sol');
+// const DF = artifacts.require('DFToken.sol');
 
-module.exports = async function(deployer, network, accounts) {
+module.exports = async function (deployer, network, accounts) {
 
     let contractFunds = await Funds.deployed();
     let contractProtocol = await Protocol.deployed();
@@ -24,7 +24,7 @@ module.exports = async function(deployer, network, accounts) {
     let contractPriceFeed = await PriceFeed.deployed();
     let contractMedianizer = await Medianizer.deployed();
     let contractUSDx = await USDx.deployed();
-    let contractDF = await DF.deployed();
+    // let contractDF = await DF.deployed();
 
     let count = 0
 
@@ -37,8 +37,8 @@ module.exports = async function(deployer, network, accounts) {
         console.log(`\n-#######`, str);
     }
 
-    function error(str) {
-        console.log(`\n!!!!!!!`, str); 
+    function perror(str) {
+        console.log(`\n!!!!!!!`, str);
     }
 
     // USDx
@@ -46,121 +46,151 @@ module.exports = async function(deployer, network, accounts) {
         print("contractUSDx.setAuthority");
         printTx(result.tx);
     }).catch(error => {
-        error("contractUSDx.setAuthority")
+        perror("contractUSDx.setAuthority")
     })
 
-    await contractUSDx.mint.sendTransaction(accounts[0], 0).then(result => {
-        print("contractUSDx.mint");
-        printTx(result.tx);
-    }).catch(error => {
-        error("contractUSDx.mint")
-    })
+    // // DF
+    // await contractDF.setAuthority.sendTransaction(contarctEngine.address).then(result => {
+    //     print("contractDF.setAuthority");
+    //     printTx(result.tx);
+    // }).catch(error => {
+    //     perror("contractDF.setAuthority")
+    // })
 
-    // DF
-    await contractDF.setAuthority.sendTransaction(contarctEngine.address).then(result => {
-        print("contractDF.setAuthority");
-        printTx(result.tx);
-    }).catch(error => {
-        error("contractDF.setAuthority")
-    })
+    // // Mint DF
+    // let ten = new BN(Number(1000000000 * 10 ** 18).toLocaleString().replace(/,/g, ''));
+    // await contractDF.mint.sendTransaction(accounts[0], ten).then(result => {
+    //     print("contractDF.mint");
+    //     printTx(result.tx);
+    // }).catch(error => {
+    //     perror("contractDF.mint")
+    // })
 
-    let ten = new BN(Number(10000000000 * 10 ** 18).toLocaleString().replace(/,/g,''));
-
-    await contractDF.mint.sendTransaction(accounts[0], ten).then(result => {
-        print("contractDF.mint");
-        printTx(result.tx);
-    }).catch(error => {
-        error("contractDF.mint")
-    })
-
-    // store
+    // Set guard => Store 
     await contractStore.setAuthority.sendTransaction(contractGuard.address).then(result => {
         print("contractStore.setAuthority");
         printTx(result.tx);
     }).catch(error => {
-        error("contractStore.setAuthority")
+        perror("contractStore.setAuthority")
     })
 
-    // Pool
+    // Set guard => Pool
     await contractPool.setAuthority.sendTransaction(contractGuard.address).then(result => {
         print("contractPool.setAuthority");
         printTx(result.tx);
     }).catch(error => {
-        error("contractPool.setAuthority")
+        perror("contractPool.setAuthority")
     })
 
-    // Collateral
+    // Set guard => Collateral
     await contractCollateral.setAuthority.sendTransaction(contractGuard.address).then(result => {
         print("contractCollateral.setAuthority");
         printTx(result.tx);
     }).catch(error => {
-        error("contractCollateral.setAuthority")
+        perror("contractCollateral.setAuthority")
     })
 
-    // Funds
+    // Set guard => Funds
     await contractFunds.setAuthority.sendTransaction(contractGuard.address).then(result => {
         print("contractFunds.setAuthority");
         printTx(result.tx);
     }).catch(error => {
-        error("contractFunds.setAuthority")
+        perror("contractFunds.setAuthority")
     })
 
-    // Engine
+    // Set guard => Engine
     await contarctEngine.setAuthority.sendTransaction(contractGuard.address).then(result => {
         print("contarctEngine.setAuthority");
         printTx(result.tx);
     }).catch(error => {
-        error("contarctEngine.setAuthority")
+        perror("contarctEngine.setAuthority")
     })
 
-    // Guard
+    // Store permit => Engine
     await contractGuard.permitx.sendTransaction(contarctEngine.address, contractStore.address).then(result => {
         print("contractGuard.permitx Store");
         printTx(result.tx);
     }).catch(error => {
-        error("contractGuard.permitx Store")
+        perror("contractGuard.permitx Store")
     })
 
+    // Pool permit => Engine
     await contractGuard.permitx.sendTransaction(contarctEngine.address, contractPool.address).then(result => {
         print("contractGuard.permitx Pool");
         printTx(result.tx);
     }).catch(error => {
-        error("contractGuard.permitx Pool")
+        perror("contractGuard.permitx Pool")
     })
 
+    // Collateral permit => Engine
     await contractGuard.permitx.sendTransaction(contarctEngine.address, contractCollateral.address).then(result => {
         print("contractGuard.permitx Collateral");
         printTx(result.tx);
     }).catch(error => {
-        error("contractGuard.permitx Collateral")
+        perror("contractGuard.permitx Collateral")
     })
 
+    // Funds permit => Engine
     await contractGuard.permitx.sendTransaction(contarctEngine.address, contractFunds.address).then(result => {
         print("contractGuard.permitx Funds");
         printTx(result.tx);
     }).catch(error => {
-        error("contractGuard.permitx Funds")
+        perror("contractGuard.permitx Funds")
     })
 
+    // Engine permit => Protocol
     await contractGuard.permitx.sendTransaction(contractProtocol.address, contarctEngine.address).then(result => {
         print("contractGuard.permitx Engine");
         printTx(result.tx);
     }).catch(error => {
-        error("contractGuard.permitx Engine")
+        perror("contractGuard.permitx Engine")
     })
 
-    // Protocol
+    // Protocol set Engine instance
     await contractProtocol.requestImplChange.sendTransaction(contarctEngine.address).then(result => {
         print("contractProtocol.requestImplChange");
         printTx(result.tx);
     }).catch(error => {
-        error("contractProtocol.requestImplChange")
+        perror("contractProtocol.requestImplChange")
     })
 
     await contractProtocol.confirmImplChange.sendTransaction().then(result => {
         print("contractProtocol.confirmImplChange");
         printTx(result.tx);
     }).catch(error => {
-        error("contractProtocol.confirmImplChange")
+        perror("contractProtocol.confirmImplChange")
+    })
+
+    //Set commission rate deposit ==> 0
+    await contarctEngine.setCommissionRate.sendTransaction(0, 0).then(result => {
+        print("contarctEngine.setCommissionRate");
+        printTx(result.tx);
+    }).catch(error => {
+        perror("contarctEngine.setCommissionRate")
+    })
+
+    //Set commission rate destroy ==> 0.005
+    await contarctEngine.setCommissionRate.sendTransaction(1, 50).then(result => {
+        print("contarctEngine.setCommissionRate");
+        printTx(result.tx);
+    }).catch(error => {
+        perror("contarctEngine.setCommissionRate")
+    })
+
+    // Medianizer
+    await contractMedianizer.set.sendTransaction(contractPriceFeed.address).then(result => {
+        print("MedianizerSetFeed.post");
+        printTx(result.tx);
+    }).catch(error => {
+        perror("MedianizerSetFeed.post")
+    })
+
+    // PriceFeed
+    let price = new BN(Number(5.88 * 10 ** 18).toLocaleString().replace(/,/g, ''));
+    await contractPriceFeed.post.sendTransaction(price, 2058870102, contractMedianizer.address).then(result => {
+        print("contractPriceFeed.post");
+        printTx(result.tx);
+    }).catch(error => {
+        perror("contractPriceFeed.post")
     })
 };
