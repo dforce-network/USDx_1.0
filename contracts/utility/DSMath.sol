9d067ee0d49cd1b8ae328fce01f554700a65c54a
l@dforce.network <l@dforce.network>
2019-06-14 10:33:58 +0800

    add math power method

--- contracts/utility/DSMath.sol
+++ contracts/utility/DSMath.sol
@@ -87,4 +87,25 @@ contract DSMath {
             }
         }
     }
+
+    /**
+     * @dev x to the power of y
+     */
+    function pwr(uint256 x, uint256 y)
+        public
+        pure
+        returns (uint256)
+    {
+        if (x==0)
+            return (0);
+        else if (y==0)
+            return (1);
+        else
+        {
+            uint256 z = x;
+            for (uint256 i = 1; i < y; i++)
+                z = mul(z,x);
+            return (z);
+        }
+    }
 }
