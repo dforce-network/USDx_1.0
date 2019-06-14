877fc35e91e613a941f0256324dbf71b6e505713
l@dforce.network <l@dforce.network>
2019-06-14 14:13:22 +0800

    fix migrations 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -11,10 +11,7 @@ const PriceFeed = artifacts.require('PriceFeed.sol');
 const Medianizer = artifacts.require('Medianizer.sol');
 const USDx = artifacts.require('USDXToken.sol');
 const Setting = artifacts.require('DFSetting.sol');
-const xDAI = artifacts.require('DSWrappedToken.sol');
-const xPAX = artifacts.require('DSWrappedToken.sol');
-const xTUSD = artifacts.require('DSWrappedToken.sol');
-const xUSDC = artifacts.require('DSWrappedToken.sol');
+const DSWrappedToken = artifacts.require('DSWrappedToken.sol');
 // const DF = artifacts.require('DFToken.sol');
 
 // const USDx_Addr = "0x17996ea27d03d68ddc618f9b8f0faf43838acaf6";
@@ -30,10 +27,18 @@ module.exports = async function (deployer, network, accounts) {
     // await deployer.deploy(DF, "0x6b6600000000000000000000000000");
     await deployer.deploy(Guard);
     await deployer.deploy(Protocol);
-    await deployer.deploy(xDAI, '0xf494e07dfdbce883bf699cedf818fde2fa432db4', 18, "0x6b6b00000000000000000000000000");
-    await deployer.deploy(xPAX, '0x2901ea287e0299d595783faedae3ca0ab2bc4e53', 12, "0x6b6c00000000000000000000000000");
-    await deployer.deploy(xTUSD, '0xfb010ff66700b6ace85fa68e2d98ab754b6f7af4', 8, "0x6b6a00000000000000000000000000");
-    await deployer.deploy(xUSDC, '0x481f8ff13489695b2e1c81691a95a81f8cb96e32', 6, "0x6b6800000000000000000000000000");
+    
+var xDAI  = await deployer.deploy(DSWrappedToken, '0xf494e07dfdbce883bf699cedf818fde2fa432db4', 18, "0x6b6b00000000000000000000000000");
+var xPAX  = await deployer.deploy(DSWrappedToken, '0x2901ea287e0299d595783faedae3ca0ab2bc4e53', 12, "0x6b6c00000000000000000000000000");
+var xTUSD = await deployer.deploy(DSWrappedToken, '0xfb010ff66700b6ace85fa68e2d98ab754b6f7af4', 8, "0x6b6a00000000000000000000000000"); 
+var xUSDC = await deployer.deploy(DSWrappedToken, '0x481f8ff13489695b2e1c81691a95a81f8cb96e32', 6, "0x6b6800000000000000000000000000"); 
+    
+    console.log('----------------------------------\n');
+    console.log('xDAI address : ' + xDAI.address);
+    console.log('xPAX address : ' + xPAX.address);
+    console.log('xTUSD address : ' + xTUSD.address);
+    console.log('xUSDC address : ' + xUSDC.address);
+    console.log('----------------------------------\n');
     await deployer.deploy(Store,
         [
             '0xf494e07dfdbce883bf699cedf818fde2fa432db4',
