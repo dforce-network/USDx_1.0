78ff428547546f8c7db29c7cad1bd778ad0e2d2e
snow <snow@dforce.network>
2019-06-18 13:24:18 +0800

    update example.

--- webdemo/example.js
+++ webdemo/example.js
@@ -4,10 +4,11 @@ var protocolAddr = "0x6507Cc2fFE3e4465499850404778749D412F5d25";
 var daiAddr = "0xf494e07dfdbce883bf699cedf818fde2fa432db4";
 var engineAddr = "0x85009C7dBbd497D908d82CcDb5c3Ac4949c0d4CD";
 var poolAddr = "0xb8925aA1Ac58Ba598D7756b39445C03f1cda82B2";
+var owner = "0x862AE9d72Da6B0cf93c58fCbD70e5d1172b3E7A0"; // _change_it_ to yours.
 var contractUsdx = web3.eth.contract(usdxABI).at(usdxAddr);
 var contractDF = web3.eth.contract(erc20ABI).at(dfAddr);
 var contractProtocol = web3.eth.contract(protocolABI).at(protocolAddr);
-var contractDAI = web3.eth.contract(erc20ABI).at(daiAddr);
+var contractDAI = web3.eth.contract(faucetERC20ABI).at(daiAddr);
 
 function usdxBurnApprove() {
   contractUsdx.approvex.sendTransaction(engineAddr, {
@@ -39,6 +40,18 @@ function tryMintingSection() {
     function (err, ret) {
       console.log(ret[1][0].toFixed(), err);
       console.log(ret[1][1].toFixed(), err);
+    }
+  );
+}
+
+function faucetDAI() {
+  contractDAI.allocateTo.sendTransaction(
+    owner,
+    10000 * 1000000000000000000, {
+      gas: 1000000
+    },
+    function (err, ret) {
+      console.log(ret, err);
     }
   );
 }
