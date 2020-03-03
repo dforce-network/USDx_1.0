9e339b00c9239202091f840524ccc0a9ded83275
Snow Ji <sj@mac.com>
2020-03-04 07:58:52 +0800

    clean & update DFProtocolView contract.

--- web_react/src/abi/addressMap.js
+++ web_react/src/abi/addressMap.js
@@ -1,31 +1,3 @@
-// const addressMap = {
-//     '1': {
-//         'addressDAI': '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359'.toLowerCase(),
-//         'addressPAX': '0x8e870d67f660d95d5be530380d0ec0bd388289e1'.toLowerCase(),
-//         'addressTUSD': '0x0000000000085d4780B73119b644AE5ecd22b376'.toLowerCase(),
-//         'addressUSDC': '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'.toLowerCase(),
-//         'addressDF': '0x431ad2ff6a9c365805ebad47ee021148d6f7dbe0'.toLowerCase(),
-//         'addressUSDx': '0xeb269732ab75a6fd61ea60b06fe994cd32a83549'.toLowerCase(),
-//         'addressProtocol': '0x5843f1ccc5baa448528eb0e8bc567cda7ed1a1e8'.toLowerCase(),
-//         'addressProtocolView': '0x1351e109fb51888d8e9dd6b364297998b36bf55e'.toLowerCase(),
-//         'addressEngine': '0x8dc753d30b9f9f86dac3380e37d5bc03d6b60202'.toLowerCase(),
-//         'addressPool': '0x786bf554473f9ab733fd683c528212492a23d895'.toLowerCase(),
-//     },
-//     '4': {
-//         'addressDAI': '0x506243424a778382f73bf6c24390d08fa9096092'.toLowerCase(),
-//         'addressPAX': '0xd414e78d5db39e90c704070943e067ffc0eb3d86'.toLowerCase(),
-//         'addressTUSD': '0xfeb2112e370091f25a2f96fb600484700a0ed603'.toLowerCase(),
-//         'addressUSDC': '0x71abccd90dbb09c37686e4d5026c2d9597d469cb'.toLowerCase(),
-//         'addressDF': '0x5d378961e9d31c0ee394d34741fa1a18144f6fb5'.toLowerCase(),
-//         'addressUSDx': '0xdbcfff49d5f48ddf6e6df1f2c9b96e1fc0f31371'.toLowerCase(),
-//         'addressProtocol': '0x7ca26e42bba5543cf5a06a0d554a0fc607f3d4ac'.toLowerCase(),
-//         'addressProtocolView': '0xaf47a307ea7d5dc7133b309c1a76c6a3f4ae1618'.toLowerCase(),
-//         'addressEngine': '0x03463c0ac738a370e0cd8c4e70a9be183255f25d'.toLowerCase(),
-//         'addressPool': '0xccf31dc9dcb6cb788d3c6b64f73efedfb7e9f20b'.toLowerCase(),
-//     }
-// };
-
-// main net TEST
 const addressMap = {
     '1': {
         'addressDAI': '0x3cb8eb28e1ebfba8eedb12233f011b067ad78f8e'.toLowerCase(),
@@ -35,7 +7,7 @@ const addressMap = {
         'addressDF': '0x6f407358319567621eab5c02c04aa4b82c1ee957'.toLowerCase(),
         'addressUSDx': '0x1cbd0c8d16dd9c8f450c781b5c3e3623f95f7344'.toLowerCase(),
         'addressProtocol': '0x540a4bc5b14d2a1b81de8e893bf75e2bac585ed9'.toLowerCase(),
-        'addressProtocolView': '0xce14792a280b20c4f8e1ae76805a6dfbe95729f5'.toLowerCase(),
+        'addressProtocolView': '0x8a7624e1715098C79B7b61c7AE5563678Ce83c6B'.toLowerCase(),
         'addressEngine': '0x37561992f92c6437f14454d4ad36b0eefe107f19'.toLowerCase(),
         'addressPool': '0xaf54dcb325b6160871f1778c3043a1249bd9b1c1'.toLowerCase(),
         'dispatcher_USDC': '0x5e71906b4B556f5c09813b816c0DC0f25a35ceA5'.toLowerCase(),
@@ -50,7 +22,7 @@ const addressMap = {
         'addressDF': '0x5d378961e9d31c0ee394d34741fa1a18144f6fb5'.toLowerCase(),
         'addressUSDx': '0xdbcfff49d5f48ddf6e6df1f2c9b96e1fc0f31371'.toLowerCase(),
         'addressProtocol': '0x7ca26e42bba5543cf5a06a0d554a0fc607f3d4ac'.toLowerCase(),
-        'addressProtocolView': '0xaf47a307ea7d5dc7133b309c1a76c6a3f4ae1618'.toLowerCase(),
+        'addressProtocolView': '0x6fee0220fa27c86b65b800819cb258cd4a8ae7b7'.toLowerCase(),
         'addressEngine': '0x03463c0ac738a370e0cd8c4e70a9be183255f25d'.toLowerCase(),
         'addressPool': '0xccf31dc9dcb6cb788d3c6b64f73efedfb7e9f20b'.toLowerCase(),
         'dispatcher_USDC': '0x41A2d0B2086243cbEf46d16c6afEA2fAB56eBF4E'.toLowerCase(),
