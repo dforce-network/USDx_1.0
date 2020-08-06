77d61bd4a98fd9bfe6ae74a63c9eb52952b3d3bf
gaoyang <gy@gmail.com>
2020-08-06 15:12:46 +0800

    update rinkeby address

--- web_react/src/abi/addressMap.js
+++ web_react/src/abi/addressMap.js
@@ -13,13 +13,13 @@ const addressMap = {
     },
     '4': {
         'addressDAI': '0x506243424a778382f73bf6c24390d08fa9096092'.toLowerCase(),
-        'addressPAX': '0xd414e78d5db39e90c704070943e067ffc0eb3d86'.toLowerCase(),
-        'addressTUSD': '0xfeb2112e370091f25a2f96fb600484700a0ed603'.toLowerCase(),
-        'addressUSDC': '0x4dbcdf9b62e891a7cec5a2568c3f4faf9e8abe2b'.toLowerCase(),
+        'addressPAX': '0x722E6238335d89393A42e2cA316A5fb1b8B2EB55'.toLowerCase(),
+        'addressTUSD': '0xe72a3181f69Eb21A19bd4Ce19Eb68FDb333d74c6'.toLowerCase(),
+        'addressUSDC': '0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b'.toLowerCase(),
         'addressDF': '0x5d378961e9d31c0ee394d34741fa1a18144f6fb5'.toLowerCase(),
         'addressUSDx': '0xdbcfff49d5f48ddf6e6df1f2c9b96e1fc0f31371'.toLowerCase(),
         'addressProtocol': '0x7ca26e42bba5543cf5a06a0d554a0fc607f3d4ac'.toLowerCase(),
-        'addressProtocolView': '0x6fee0220fa27c86b65b800819cb258cd4a8ae7b7'.toLowerCase(),
+        'addressProtocolView': '0xaf47a307ea7d5dc7133b309c1a76c6a3f4ae1618'.toLowerCase(),
         'addressEngine': '0x03463c0ac738a370e0cd8c4e70a9be183255f25d'.toLowerCase(),
         'addressPool': '0xccf31dc9dcb6cb788d3c6b64f73efedfb7e9f20b'.toLowerCase(),
     }
